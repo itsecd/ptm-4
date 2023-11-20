8138ae75d4df4d7de4c2fe244f17f1fb421dbc5f
Santas7 <andreypanyshkin03@gmail.com>
2023-11-20 21:30:46 +0400

    привел функцию setup_logger() в рабочее состояние + добавил docstring везде, убрал лишние пробелы

--- main.py
+++ main.py
@@ -8,18 +8,33 @@ class ShopInventory:
         self.logger = self.setup_logger()
 
     def setup_logger(self):
+        """
+        функция установки logger
+        :return:
+        """
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-
         file_handler = logging.FileHandler('inventory_log.log')
         file_handler.setLevel(logging.DEBUG)
-
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
         return logger
 
     def add_item(self, item: str) -> None:
+        """
+        функция добавления элемента в инвентарь
+        :param item: элемент
+        :return: None
+        """
         self.inventory.append(item)
 
     def remove_item(self, item: str) -> None:
+        """
+        функция удаления элемента из инвентаря
+        :param item: элемент
+        :return: None
+        """
         if item in self.inventory:
             self.inventory.remove(item)
             print(f"Товар удален: {item}")
@@ -27,15 +42,28 @@ class ShopInventory:
             print("error!")
 
     def display_inventory(self) -> None:
+        """
+        функция отображения инвентаря
+        :return: None
+        """
         for item in self.inventory:
             print(item)
 
     def count_items(self) -> int:
+        """
+        функция возвращает общее кол-во товаров
+        :return: int
+        """
         count = len(self.inventory)
         self.logger.info(f"Общее количество товаров: {count}")
         return count
 
     def check_item_exists(self, item: str) -> bool:
+        """
+        функция проверки на сущ-е элемента
+        :param item: str
+        :return: bool
+        """
         exists = item in self.inventory
         if exists:
             print(f"Товар '{item}' присутствует в инвентаре.")
@@ -44,6 +72,11 @@ class ShopInventory:
         return exists
 
     def save_to_csv(self, filename: str = 'inventory.csv') -> None:
+        """
+        функция сохранения данных в csv файл
+        :param filename: название файла csv
+        :return: None
+        """
         try:
             with open(filename, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
@@ -54,6 +87,11 @@ class ShopInventory:
             print(f"Файл {filename} не найден!")
 
     def load_from_csv(self, filename='inventory.csv') -> None:
+        """
+        функция чтения данных из csv файла
+        :param filename: название файла csv
+        :return: None
+        """
         try:
             with open(filename, 'r') as csvfile:
                 reader = csv.reader(csvfile)
@@ -63,6 +101,10 @@ class ShopInventory:
             print(f"Файл {filename} не найден!")
 
     def bubble_sort_inventory(self):
+        """
+        функция сортировка пузырьком инвентаря
+        :return: None
+        """
         n = len(self.inventory)
         for i in range(n - 1):
             for j in range(0, n - i - 1):
@@ -71,6 +113,11 @@ class ShopInventory:
         print("Инвентарь отсортирован методом сортировки пузырьком.")
 
     def search_item_by_name(self, partial_name: str) -> list[str]:
+        """
+        функция поиска товара с частью названия
+        :param partial_name: str
+        :return: list[str]
+        """
         matching_items = [item for item in self.inventory if partial_name.lower() in item.lower()]
         if matching_items:
             print(f"Найдены товары с частью названия '{partial_name}': {matching_items}")
@@ -78,7 +125,11 @@ class ShopInventory:
             print(f"Товары с частью названия '{partial_name}' не найдены.")
         return matching_items
 
-    def clear_inventory(self):
+    def clear_inventory(self) -> None:
+        """
+        функция очистки инвентаря
+        :return: None
+        """
         self.inventory = []
         self.logger.info("Инвентарь очищен.")
 
@@ -93,10 +144,8 @@ if __name__ == "__main__":
         shop.add_item("Обувь")
         shop.add_item("Вода")
         shop.add_item("Лопата)")
-
         shop.display_inventory()
         shop.count_items()
-
         shop.search_item_by_name("Обувь")
         shop.search_item_by_name("ноутбук")
         shop.search_item_by_name("Яблоко")
@@ -104,6 +153,5 @@ if __name__ == "__main__":
         shop.clear_inventory()
         shop.display_inventory()
         shop.count_items()
-
     except Exception as e:
         logging.exception("Произошла ошибка", exc_info=True)
