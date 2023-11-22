a07ac785c42550c614956a67a790302d04de6ea0
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2023-11-22 16:19:28 +0300

    Провел работу с кодом, иправил ошибки пеп 8, добавил докстринги и тайпхинты, сделал логер работоспособным

--- main.py
+++ main.py
@@ -1,78 +1,152 @@
 import logging
 import json
 
+
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int) -> None:
+        """
+        Инициализация объекта Person.
+        :param name: Имя персоны.
+        :param age: Возраст персоны.
+        """
         self.name = name
         self.age = age
         self.owned_books = []
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Возвращает строковое представление объекта Person.
+        :return: Строковое представление объекта Person.
+        """
         return f"{self.name}, Возраст: {self.age}"
 
+
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title: str, author: str) -> None:
+        """
+        Инициализация объекта Book.
+        :param title: Название книги.
+        :param author: Автор книги.
+        """
         self.title = title
         self.author = author
         self.owner = None
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Возвращает строковое представление объекта Book.
+        :return: Строковое представление объекта Book.
+        """
         if self.owner:
             return f"{self.title} (Автор: {self.author}, Владелец: {self.owner.name})"
         else:
             return f"{self.title} (Автор: {self.author}, Владелец: Нет)"
 
+
 class Library:
     def __init__(self) -> None:
+        """
+        Инициализация объекта Library.
+        """
         self.people = []
         self.books = []
         self.logger = self.setup_logger()
 
-    def setup_logger(self):
+    @staticmethod
+    def setup_logger():
+        """
+        Настройка логгера.
+        :return: Объект логгера.
+        """
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
         file_handler = logging.FileHandler('library_log.txt')
-        #file_handler.setLevel(logging.DEBUG)
+        file_handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
-        #logger.addHandler(file_handler)
+        logger.addHandler(file_handler)
         return logger
 
     def add_person(self, person: Person) -> None:
+        """
+        Добавляет персону в библиотеку.
+        :param person: Объект Person.
+        :return: None
+        """
         self.people.append(person)
+        self.logger.info(f"Добавлена персона: {person}")
 
     def remove_person(self, person: Person) -> None:
+        """
+        Удаляет персону из библиотеки.
+        :param person: Объект Person.
+        :return: None
+        """
         if person in self.people:
             self.people.remove(person)
+            self.logger.info(f"Персона удалена: {person}")
         else:
-            print(f"Попытка удалить несуществующую персону: {person}")
+            self.logger.warning(f"Попытка удалить несуществующую персону: {person}")
 
     def add_book(self, book: Book) -> None:
+        """
+        Добавляет книгу в библиотеку.
+        :param book: Объект Book.
+        :return: None
+        """
         self.books.append(book)
+        self.logger.info(f"Добавлена книга: {book}")
 
     def remove_book(self, book: Book) -> None:
+        """
+        Удаляет книгу из библиотеки.
+        :param book: Объект Book.
+        :return: None
+        """
         if book in self.books:
             self.books.remove(book)
+            self.logger.info(f"Книга удалена: {book}")
         else:
-            print(f"Попытка удалить несуществующую книгу: {book}")
+            self.logger.warning(f"Попытка удалить несуществующую книгу: {book}")
 
     def lend_book(self, book: Book, person: Person) -> None:
+        """
+        Выдает книгу в аренду персоне.
+        :param book: Объект Book.
+        :param person: Объект Person.
+        :return: None
+        """
         if book in self.books and person in self.people:
             book.owner = person
             person.owned_books.append(book)
+            self.logger.info(f"{person} взял в аренду книгу: {book}")
         else:
-            print(f"Невозможно выдать книгу {book} в аренду {person}")
+            self.logger.warning(f"Невозможно выдать книгу {book} в аренду {person}")
 
     def display_people(self) -> None:
+        """
+        Выводит информацию о людях в библиотеке.
+        :return: None
+        """
         self.logger.info("Вывод информации о людях:")
         for person in self.people:
-            print(person)
+            self.logger.debug(person)
 
     def display_books(self) -> None:
+        """
+        Выводит информацию о книгах в библиотеке.
+        :return: None
+        """
+        self.logger.info("Вывод информации о книгах:")
         for book in self.books:
-            print(book)
+            self.logger.debug(book)
 
     def save_to_txt(self, filename: str = 'library.txt') -> None:
+        """
+        Сохраняет информацию о библиотеке в текстовый файл.
+        :param filename: Название файла.
+        :return: None
+        """
         data = {
             'people': [str(person) for person in self.people],
             'books': [str(book) for book in self.books]
@@ -80,10 +154,16 @@ class Library:
         try:
             with open(filename, 'w') as txtfile:
                 json.dump(data, txtfile, indent=2)
+            self.logger.info(f"Данные о библиотеке сохранены в файл: {filename}")
         except Exception as e:
-            print(f"Ошибка при сохранении данных о библиотеке в файл {filename}: {str(e)}")
+            self.logger.warning(f"Ошибка при сохранении данных о библиотеке в файл {filename}: {str(e)}")
 
     def load_from_txt(self, filename: str = 'library.txt') -> None:
+        """
+        Загружает информацию о библиотеке из текстового файла.
+        :param filename: Название файла.
+        :return: None
+        """
         try:
             with open(filename, 'r') as txtfile:
                 data = json.load(txtfile)
@@ -93,45 +173,38 @@ class Library:
                 for book_str in data['books']:
                     title_author, owner = [item.strip() for item in book_str.split(' (Владелец: ')]
                     title, author = [item.strip() for item in title_author.split(' (Автор: ')]
-                    owner = owner[:-1]  # Удаляем последний символ ')' из owner
+                    owner = owner[:-1]
                     book = Book(title, author)
                     self.add_book(book)
                     if owner != "Нет":
                         owner_person = next((person for person in self.people if person.name == owner), None)
                         if owner_person:
                             self.lend_book(book, owner_person)
-            print(f"Данные о библиотеке загружены из файла: {filename}")
+            self.logger.info(f"Данные о библиотеке загружены из файла: {filename}")
         except FileNotFoundError:
-            print(f"Файл {filename} не найден.")
+            self.logger.warning(f"Файл {filename} не найден.")
         except Exception as e:
-            print(f"Ошибка при загрузке данных о библиотеке из файла {filename}: {str(e)}")
+            self.logger.warning(f"Ошибка при загрузке данных о библиотеке из файла {filename}: {str(e)}")
+
 
 if __name__ == "__main__":
     try:
         library = Library()
-
         person1 = Person("Alice", 25)
         person2 = Person("Bob", 30)
-
         book1 = Book("The Great Gatsby", "F. Scott Fitzgerald")
         book2 = Book("To Kill a Mockingbird", "Harper Lee")
-
         library.add_person(person1)
         library.add_person(person2)
         library.add_book(book1)
         library.add_book(book2)
-
         library.lend_book(book1, person1)
         library.lend_book(book2, person2)
-
         library.display_people()
         library.display_books()
-
         library.save_to_txt()
         library.load_from_txt()
-
         library.display_people()
         library.display_books()
-
-    except Exception as e:
-        print("Произошла ошибка")
+    except ValueError as e:
+        logging.exception("Произошла ошибка", exc_info=True)
