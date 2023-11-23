f587957d592a9fd1a42ee89a93b71c1c2511fc90
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2023-11-23 13:17:56 +0300

    Сделал логи читаемыми для Гита

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Person:
         Возвращает строковое представление объекта Person.
         :return: Строковое представление объекта Person.
         """
-        return f"{self.name}, Возраст: {self.age}"
+        return f"{self.name}, Age: {self.age}"
 
 
 class Book:
@@ -38,9 +38,9 @@ class Book:
         :return: Строковое представление объекта Book.
         """
         if self.owner:
-            return f"{self.title} (Автор: {self.author}, Владелец: {self.owner.name})"
+            return f"{self.title} (Author: {self.author}, Owner: {self.owner.name})"
         else:
-            return f"{self.title} (Автор: {self.author}, Владелец: Нет)"
+            return f"{self.title} (Author: {self.author}, Owner: No)"
 
 
 class Library:
@@ -74,7 +74,7 @@ class Library:
         :return: None
         """
         self.people.append(person)
-        self.logger.info(f"Добавлена персона: {person}")
+        self.logger.info(f"Person added: {person}")
 
     def remove_person(self, person: Person) -> None:
         """
@@ -84,9 +84,9 @@ class Library:
         """
         if person in self.people:
             self.people.remove(person)
-            self.logger.info(f"Персона удалена: {person}")
+            self.logger.info(f"The person has been deleted: {person}")
         else:
-            self.logger.warning(f"Попытка удалить несуществующую персону: {person}")
+            self.logger.warning(f"Attempt to delete a non-existent person: {person}")
 
     def add_book(self, book: Book) -> None:
         """
@@ -95,7 +95,7 @@ class Library:
         :return: None
         """
         self.books.append(book)
-        self.logger.info(f"Добавлена книга: {book}")
+        self.logger.info(f"Book added: {book}")
 
     def remove_book(self, book: Book) -> None:
         """
@@ -105,9 +105,9 @@ class Library:
         """
         if book in self.books:
             self.books.remove(book)
-            self.logger.info(f"Книга удалена: {book}")
+            self.logger.info(f"Book deleted: {book}")
         else:
-            self.logger.warning(f"Попытка удалить несуществующую книгу: {book}")
+            self.logger.warning(f"Attempt to delete a non-existent book: {book}")
 
     def lend_book(self, book: Book, person: Person) -> None:
         """
@@ -119,16 +119,16 @@ class Library:
         if book in self.books and person in self.people:
             book.owner = person
             person.owned_books.append(book)
-            self.logger.info(f"{person} взял в аренду книгу: {book}")
+            self.logger.info(f"{person} I rented a book: {book}")
         else:
-            self.logger.warning(f"Невозможно выдать книгу {book} в аренду {person}")
+            self.logger.warning(f"It is impossible to issue a book {book} for rent {person}")
 
     def display_people(self) -> None:
         """
         Выводит информацию о людях в библиотеке.
         :return: None
         """
-        self.logger.info("Вывод информации о людях:")
+        self.logger.info("Output of information about people:")
         for person in self.people:
             self.logger.debug(person)
 
@@ -137,7 +137,7 @@ class Library:
         Выводит информацию о книгах в библиотеке.
         :return: None
         """
-        self.logger.info("Вывод информации о книгах:")
+        self.logger.info("Output of information about books:")
         for book in self.books:
             self.logger.debug(book)
 
@@ -154,9 +154,9 @@ class Library:
         try:
             with open(filename, 'w') as txtfile:
                 json.dump(data, txtfile, indent=2)
-            self.logger.info(f"Данные о библиотеке сохранены в файл: {filename}")
+            self.logger.info(f"The library data is saved to a file: {filename}")
         except Exception as e:
-            self.logger.warning(f"Ошибка при сохранении данных о библиотеке в файл {filename}: {str(e)}")
+            self.logger.warning(f"Error when saving library data to a file {filename}: {str(e)}")
 
     def load_from_txt(self, filename: str = 'library.txt') -> None:
         """
@@ -171,20 +171,20 @@ class Library:
                     name, age = [item.strip() for item in person_str.split(',')]
                     self.add_person(Person(name, int(age)))
                 for book_str in data['books']:
-                    title_author, owner = [item.strip() for item in book_str.split(' (Владелец: ')]
-                    title, author = [item.strip() for item in title_author.split(' (Автор: ')]
+                    title_author, owner = [item.strip() for item in book_str.split(' (Owner: ')]
+                    title, author = [item.strip() for item in title_author.split(' (Author: ')]
                     owner = owner[:-1]
                     book = Book(title, author)
                     self.add_book(book)
-                    if owner != "Нет":
+                    if owner != "No":
                         owner_person = next((person for person in self.people if person.name == owner), None)
                         if owner_person:
                             self.lend_book(book, owner_person)
-            self.logger.info(f"Данные о библиотеке загружены из файла: {filename}")
+            self.logger.info(f"The library data is loaded from a file: {filename}")
         except FileNotFoundError:
-            self.logger.warning(f"Файл {filename} не найден.")
+            self.logger.warning(f"File {filename} not found.")
         except Exception as e:
-            self.logger.warning(f"Ошибка при загрузке данных о библиотеке из файла {filename}: {str(e)}")
+            self.logger.warning(f"Error loading library data from a file {filename}: {str(e)}")
 
 
 if __name__ == "__main__":
@@ -207,4 +207,4 @@ if __name__ == "__main__":
         library.display_people()
         library.display_books()
     except ValueError as e:
-        logging.exception("Произошла ошибка", exc_info=True)
+        logging.exception("An error has occurred", exc_info=True)
