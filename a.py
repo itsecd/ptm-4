4532202e06f348ec7d7e1b8d7d2c5668d842e03e
Leonid <Anevskyi2018@yandex.ru>
2023-11-26 13:58:24 +0400

    deleted the wrong file

--- a.py
+++
@@ -1,8 +0,0 @@
-with open('sentences.txt') as file:
-    sentences = list(file)
-print(sentences)
-
-print('----------------')
-f = open('sentences.txt').read()
-sentences = f.split('\n')
-print(sentences)
