0f7182252ed7cc3c168c88580cebc35107d98329
Alexey Zemel <loyxa.zemel@gmail.com>
2023-11-30 16:31:34 +0400

    добавлено логирование в файл

--- main.py
+++ main.py
@@ -1,103 +1,138 @@
 import os
+import random
 import string
 import time
-from bs4 import BeautifulSoup
+
+import logging
 import requests
-from re import sub
-import random
+from bs4 import BeautifulSoup
+from enum import Enum
 from PIL import ImageChops, Image
 from tqdm import tqdm
 
-
-if not os.path.isdir("E:\dataset"):
-    os.mkdir("E:\dataset")
-
-if not os.path.isdir("E:\dataset\\brownbears"):
-    os.mkdir("E:\dataset\\brownbears")
-
-if not os.path.isdir("E:\dataset\polarbears"):
-    os.mkdir("E:\dataset\polarbears")
-
 PBEAR_PATH = "E:\dataset\polarbears"
 BBEAR_PATH = "E:\dataset\\brownbears"
+COUNT_FIND = 1050
 
 
-def get_images(count_imgs, path, name):
-    count = 0
+class Bears(Enum):
+    POLAR_BEAR = "polar bear"
+    BROWN_BEAR = "brown bear"
 
+
+def find_images(count_imgs: int, path: str, name: str) -> None:
+    """
+    Находит картинки на Яндексе по запросу.
+
+    Аргументы:
+        count_imgs(int): количество картинок для поиска
+        path(str): путь сохранения картинок
+        name(str): имя запроса
+    """
     for i in tqdm(range(3, 999), desc="Страница ", colour="green"):
         letters = string.ascii_lowercase
         rand_string = "".join(random.sample(letters, 10))
         _headers = {"User-Agent": rand_string}
-
         url = f"https://yandex.ru/images/search?p={i}&text={name}&"
-        html_page = requests.get(url, headers=_headers)
-
-        soup = BeautifulSoup(html_page.text, "html.parser")
-
+        try:
+            html_page = requests.get(url, headers=_headers)
+            soup = BeautifulSoup(html_page.text, "html.parser")
+        except Exception as err:
+            logging.ERROR(f"Невалидный URL: {err}")
         src_list = []
-
         for link in soup.find_all("img", class_="serp-item__thumb justifier__thumb"):
             src_list.append(link.get("src"))
-
-        for img_url in tqdm(src_list, desc="Скачивание картинок ", colour="green"):
-            if img_url.find("n=13") != -1:
-                try:
-                    source = "https:" + img_url
-                    picture = requests.get(source)
-
-                    name_file = str(count)
-
-                    fpicture = open(path + "/" + name_file.zfill(4) + ".jpg", "wb")
-                    fpicture.write(picture.content)
-                    fpicture.close()
-
-                    time.sleep(0.25)
-
-                    count += 1
-                    if count == count_imgs:
-                        return
-
-                except Exception:
-                    print("Error in: ", count)
+        logging.INFO("Получены адреса картинок для загрузки.")
+        count = get_images(count_imgs, path, src_list)
+        logging.INFO(f"Загружено {count} картинок...")
+        if count == count_imgs:
+            return
 
 
-def img_compare(img1, img2):
-    image_1 = Image.open(img1)
-    image_2 = Image.open(img2)
-    result = ImageChops.difference(image_1, image_2).getbbox()
-    return result
+def get_images(count_imgs: int, path: str, src_list: list) -> int:
+    """
+    Сохраняет найденные картинки по заданному пути
+
+    Аргументы:
+        count_imgs(int): количество картинок для поиска
+        path(str): путь сохранения картинок
+        src_list(list): список адресов картинок для загрузки
+        
+    Возвращаемое значение: 
+        int: количество загруженных картинок
+    """
+    count = 0
+    for img_url in tqdm(src_list, desc="Скачивание картинок ", colour="green"):
+        if img_url.find("n=13") != -1:
+            try:
+                source = "https:" + img_url
+                picture = requests.get(source)
+                name_file = str(count)
+                fpicture = open(path + "/" + name_file.zfill(4) + ".jpg", "wb")
+                fpicture.write(picture.content)
+                fpicture.close()
+                time.sleep(0.25)
+                count += 1
+                if count == count_imgs:
+                    return count
+                return count
+            except Exception as err:
+                logging.ERROR(f"Ошибка при скачивании картинки: {err}")
 
 
-def check_images(path, count):
+def img_compare(img1: str, img2: str) -> tuple or None:
+    """
+    Сравнивает две картинки
+
+    Аргументы:
+        img1(str): путь первой картинки
+        img2(str): путь второй картинки
+
+    Возвращаемое значение:
+        tuple or None: tuple, если картинки разные, None - одинаковые
+    """
+    try:
+        image_1 = Image.open(img1)
+        image_2 = Image.open(img2)
+        result = ImageChops.difference(image_1, image_2).getbbox()
+        return result
+    except Exception as err:
+        logging.ERROR(f"Не удалось сравнить картинки: {err}")
+
+
+def check_images(path: str, count: int) -> int:
+    """
+    Проверяет, есть ли одинавковые картинки, и возвращает количество картинок для дозагрузки
+
+    Аргументы:
+        path(str): путь до картинок
+        count(int): количество картинок для загрузки
+
+    Возвращаемое значение:
+        int: количество удалённых картинок
+    """
     c = count
     images = []
     for filename1 in os.listdir(path):
         images.append(os.path.join(path, filename1))
-
     for fname1 in tqdm(images, colour="green"):
         for fname2 in images:
             if fname1 == fname2:
                 continue
             if img_compare(fname1, fname2) == None:
-                print(fname1, fname2)
                 os.remove(fname2)
+                logging.INFO(f"Найдены идентичные картинки: {fname1} and {fname2}")
                 c -= 1
     return count - c
 
 
-count_find = 1050
-
-get_images(count_find, PBEAR_PATH, "polar bear")
-
-print("Следующий этап")
-
-get_images(count_find, BBEAR_PATH, "brown bear")
-
-new_count = check_images(PBEAR_PATH, count_find)
-get_images(new_count, PBEAR_PATH, "polar bear")
-
-print("Следующий этап")
-
-new_count = check_images(BBEAR_PATH, count_find)
-get_images(new_count, BBEAR_PATH, "brown bear")
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, filename="prog_logs.log", filemode="w")
+    get_images(COUNT_FIND, PBEAR_PATH, Bears.POLAR_BEAR)
+    print("Следующий этап")
+    get_images(COUNT_FIND, BBEAR_PATH, Bears.BROWN_BEAR)
+    new_count = check_images(PBEAR_PATH, COUNT_FIND)
+    get_images(new_count, PBEAR_PATH, Bears.POLAR_BEAR)
+    print("Следующий этап")
+    new_count = check_images(BBEAR_PATH, COUNT_FIND)
+    get_images(new_count, BBEAR_PATH, Bears.BROWN_BEAR)
