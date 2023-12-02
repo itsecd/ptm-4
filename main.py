1dd0150e7f4615bbcb3b34eb3f7227f410728274
Andrei <marinin1337@gmail.com>
2023-12-02 17:06:56 +0400

    Добавлено логирование

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os.path
+import logging
 import numpy as np
 import statistics as st
 import scipy.stats as sts
 import matplotlib.pyplot as plt
 
+logging.basicConfig(filename="logs.log", level=logging.INFO,
+                    format=u'%(levelname)s  %(filename)s %(asctime)s %(message)s')
 GAMMA = 0.95
 A = 2
 SIGMA2 = 7
@@ -25,6 +28,7 @@ def find_expectation_inter(normal_sample: np.ndarray, gamma: float, sigma: float
     delta = t * sigma / np.sqrt(len(normal_sample))
     x_l = normal_sample.mean() - delta
     x_r = normal_sample.mean() + delta
+    logging.info(f'Expectation inter [{x_l}, {x_r}] with known dispersion was successfully found for gamma = {gamma}')
     return x_l, x_r
 
 
@@ -41,6 +45,7 @@ def find_expectation_inter_no_dis(normal_sample: np.ndarray, gamma: float) -> tu
     delta = t * s / np.sqrt(len(normal_sample))
     x_l = normal_sample.mean() - delta
     x_r = normal_sample.mean() + delta
+    logging.info(f'Expectation inter [{x_l}, {x_r}] with known dispersion was successfully found for gamma = {gamma}')
     return x_l, x_r
 
 
@@ -54,10 +59,14 @@ def find_dispersion(normal_sample: np.ndarray, gamma: float) -> tuple:
     """
     alpha = gamma/2 + 0.5
     s = st.pvariance(normal_sample)
-    chil = sts.chi2.ppf(alpha, len(normal_sample) - 1)
-    chir = sts.chi2.ppf(1 - alpha, len(normal_sample) - 1)
-    sg_l = (len(normal_sample) - 1) * s / chil
-    sg_r = (len(normal_sample) - 1) * s / chir
+    chi_left = sts.chi2.ppf(alpha, len(normal_sample) - 1)
+    chi_right = sts.chi2.ppf(1 - alpha, len(normal_sample) - 1)
+    sg_l = (len(normal_sample) - 1) * s / chi_left
+    sg_r = (len(normal_sample) - 1) * s / chi_right
+    if chi_left == 0 or chi_right == 0:
+        logging.warning(f'Gamma value({gamma}) might be incorrect, must be in [0, 1)')
+    else:
+        logging.info(f'Dispersion interval [{sg_l}, {sg_r}] was successfully found for gamma = {gamma}')
     return sg_l, sg_r
 
 
@@ -72,6 +81,7 @@ def plot_dependence_l_of_gamma_ex(number_of_points: int, sample_size: int, save_
     :return:
     """
     if sample_size <= 0 or number_of_points <= 0:
+        logging.error(f'One or both sample_size and numer_of_points argument is incorrect, should be > 0')
         return None
     gm = np.linspace(0, 1, number_of_points)
     sample = np.random.normal(loc=A, scale=np.sqrt(SIGMA2), size=sample_size)
@@ -85,10 +95,19 @@ def plot_dependence_l_of_gamma_ex(number_of_points: int, sample_size: int, save_
     plt.ylabel("L")
     plt.xlabel("gamma")
     plt.title(f'Зависимость L от gamma при n={len(sample)}')
-    if os.path.isdir(save_path) and os.path.exists(save_path) \
-            and (fig_name.endswith(".png") or fig_name.endswith(".jpg")):
+    if not os.path.isdir(save_path):
+        logging.error(f'save path: {save_path}, is incorrect')
+        return None
+    if not os.path.exists(save_path):
+        logging.error(f'save path: {save_path} does not exist')
+        return None
+    if not fig_name.endswith(".png") or not fig_name.endswith(".jpg"):
+        logging.error(f'Name for plot {fig_name} has incorrect extention')
+        return None
+    else:
         full_path = os.path.join(save_path, fig_name)
         fig.savefig(full_path)
+        logging.info(f'Dependence L of gamma for expectation plot was successfully saved in {full_path}')
 
 
 def plot_dependence_l_of_gamma_dis(number_of_points: int, sample_size: int, save_path: str, fig_name: str) -> None:
@@ -102,6 +121,7 @@ def plot_dependence_l_of_gamma_dis(number_of_points: int, sample_size: int, save
     :return:
     """
     if sample_size <= 0 or number_of_points <= 0:
+        logging.error(f'One or both sample_size and numer_of_points argument is incorrect, should be > 0')
         return None
     gm = np.linspace(0, 0.99, number_of_points)
     sample = np.random.normal(loc=A, scale=np.sqrt(SIGMA2), size=sample_size)
@@ -115,10 +135,19 @@ def plot_dependence_l_of_gamma_dis(number_of_points: int, sample_size: int, save
     plt.ylabel("L")
     plt.xlabel("gamma")
     plt.title(f'Зависимость L от gamma при n={len(sample)}')
-    if os.path.isdir(save_path) and os.path.exists(save_path) \
-            and (fig_name.endswith(".png") or fig_name.endswith(".jpg")):
+    if not os.path.isdir(save_path):
+        logging.error(f'save path: {save_path}, is incorrect')
+        return None
+    if not os.path.exists(save_path):
+        logging.error(f'save path: {save_path} does not exist')
+        return None
+    if not fig_name.endswith(".png") or not fig_name.endswith(".jpg"):
+        logging.error(f'Name for plot {fig_name} has incorrect extension')
+        return None
+    else:
         full_path = os.path.join(save_path, fig_name)
         fig.savefig(full_path)
+        logging.info(f'Dependence L of gamma for dispersion plot was successfully saved in {full_path}')
 
 
 def plot_dependence_l_of_n_ex(gamma: float, min_size: int, max_size: int,
@@ -133,7 +162,12 @@ def plot_dependence_l_of_n_ex(gamma: float, min_size: int, max_size: int,
     :param fig_name: имя сохраненной картинки
     :return:
     """
-    if min_size <= 0 or max_size <= min_size or number_of_points <= 0:
+    if min_size <= 0 or max_size <= min_size:
+        logging.error(f'One or both of arguments for minimum sample size ({min_size}) or maximum sample size '
+                      f'({max_size}) is incorrect, should be > 0, max size should be > min size')
+        return None
+    if number_of_points <= 0:
+        logging.error(f'number of points argument({number_of_points}) is incorrect, should be > 0')
         return None
     n_ar = np.linspace(min_size, max_size, number_of_points, dtype=int)
     len_ar = np.array([])
@@ -147,10 +181,19 @@ def plot_dependence_l_of_n_ex(gamma: float, min_size: int, max_size: int,
     plt.xlabel("N")
     plt.ylabel("L")
     plt.title(f'Зависимость L от N при gamma = {gamma}')
-    if os.path.isdir(save_path) and os.path.exists(save_path)\
-            and (fig_name.endswith(".png") or fig_name.endswith(".jpg")):
+    if not os.path.isdir(save_path):
+        logging.error(f'save path: {save_path}, is incorrect')
+        return None
+    if not os.path.exists(save_path):
+        logging.error(f'save path: {save_path} does not exist')
+        return None
+    if not fig_name.endswith(".png") or not fig_name.endswith(".jpg"):
+        logging.error(f'Name for plot {fig_name} has incorrect extension')
+        return None
+    else:
         full_path = os.path.join(save_path, fig_name)
         fig.savefig(full_path)
+        logging.info(f'Dependence L of sample size for expectation plot was successfully saved in {full_path}')
 
 
 def plot_dependence_l_of_n_dis(gamma: float, min_size: int, max_size: int,
@@ -165,7 +208,12 @@ def plot_dependence_l_of_n_dis(gamma: float, min_size: int, max_size: int,
     :param fig_name: имя сохраненной картинки
     :return:
     """
-    if min_size <= 0 or max_size <= min_size or number_of_points <= 0:
+    if min_size <= 0 or max_size <= min_size:
+        logging.error(f'One or both of arguments for minimum sample size ({min_size}) or maximum sample size '
+                      f'({max_size}) is incorrect, should be > 0, max size should be > min size')
+        return None
+    if number_of_points <= 0:
+        logging.error(f'number of points argument({number_of_points}) is incorrect, should be > 0')
         return None
     n_ar = np.linspace(min_size, max_size, number_of_points, dtype=int)
     len_ar = np.array([])
@@ -179,10 +227,19 @@ def plot_dependence_l_of_n_dis(gamma: float, min_size: int, max_size: int,
     plt.xlabel("N")
     plt.ylabel("L")
     plt.title(f'Зависимость L от N при gamma = {gamma}')
-    if os.path.isdir(save_path) and os.path.exists(save_path) \
-            and (fig_name.endswith(".png") or fig_name.endswith(".jpg")):
+    if not os.path.isdir(save_path):
+        logging.error(f'save path: {save_path}, is incorrect')
+        return None
+    if not os.path.exists(save_path):
+        logging.error(f'save path: {save_path} does not exist')
+        return None
+    if not fig_name.endswith(".png") or not fig_name.endswith(".jpg"):
+        logging.error(f'Name for plot {fig_name} has incorrect extension')
+        return None
+    else:
         full_path = os.path.join(save_path, fig_name)
         fig.savefig(full_path)
+        logging.info(f'Dependence L of sample size for dispersion plot was successfully saved in {full_path}')
 
 
 def find_emp_gamma(m: int) -> float:
@@ -192,6 +249,7 @@ def find_emp_gamma(m: int) -> float:
     :return: точечная оценка доверительной вероятности, -1 в случае ошибки
     """
     if m <= 0:
+        logging.error(f'Incorrect number of tests({m}), should be > 0')
         return -1
     counter = 0
     for i in range(m):
@@ -199,4 +257,19 @@ def find_emp_gamma(m: int) -> float:
         left, right = find_expectation_inter_no_dis(sample, GAMMA)
         if left <= A <= right:
             counter += 1
+    logging.info("Empirical value of gamma was successfully found")
     return counter/m
+
+
+if __name__ == "__main__":
+    sample = np.random.normal(loc=A, scale=np.sqrt(SIGMA2), size=N)
+    find_expectation_inter(sample, gamma=GAMMA, sigma=np.sqrt(SIGMA2))
+    find_expectation_inter_no_dis(sample, GAMMA)
+    find_dispersion(sample, GAMMA)
+    find_dispersion(sample, 1)
+    find_emp_gamma(M)
+    find_emp_gamma(-1)
+    plot_dependence_l_of_n_dis(GAMMA, 5, 100, 500, "D:", "fg.png")
+    plot_dependence_l_of_n_dis(GAMMA, 10, 5, 500, "D:", "fg.png")
+    plot_dependence_l_of_gamma_ex(1000, 1000, "R//", "fg.pnb")
+    plot_dependence_l_of_gamma_ex(1000, 1000, "D:", "fg134")
