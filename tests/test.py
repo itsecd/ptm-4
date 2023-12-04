9df1cf7b5bd0d97fef54cb14bf609083fee8e8f9
diana <diana@DIANA_LAPTOP>
2023-12-04 14:19:37 +0400

    Исходный код

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+import curses
+
+
+def teste(screen):
+    curses.curs_set(False)
+    screen.clear()
+
+    while 1:
+
+        key = screen.getch()
+        screen.addstr(5, 5, f"Tecla precionada: {key}")
+        screen.refresh()
+
+
+curses.wrapper(teste)
