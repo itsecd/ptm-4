d722338d580a4d56785ea9f3c9aa9a1653ede566
the80hz <dnl.vlgv@mail.ru>
2023-12-10 15:06:06 +0400

    made logging for auto.py translated to eng

--- auto.py
+++ auto.py
@@ -1,54 +1,41 @@
+import logging
 import os
-import time
-from typing import NoReturn
-
-from rich import print
-from rich.console import Console
 
 
-console = Console()
+logging.basicConfig(filename='auto.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def auto() -> NoReturn:
+def auto():
     """
-    Проводит первоначальную настройку бота, включая создание директории и запись токена.
-    Если директория уже существует или токен уже записан, пропускает соответствующие шаги.
+    Conducts initial bot setup, including directory creation and token writing.
+    Skips corresponding steps if the directory already exists or the token is already written.
     """
-    print('Made by rus152')
-    print('Файл токена не найден. Начинается первоначальная настройка')
-
+    logging.info('Initial setup started')
     turnoff_path = os.path.join(os.getenv('APPDATA'), 'TurnOffBot')
+
     try:
         os.mkdir(turnoff_path)
-    except (IOError, Exception):
-        print()
+        logging.info('Directory created successfully')
+    except Exception as e:
+        logging.error(f'Error creating directory: {e}')
 
-    token_count: int = 0
-    entered_token = ''
+    token_count = 0
     while token_count < 3:
-        print('Введете свой токен. (Взять токен для своего бота можно у официального бота BotFather)')
-        entered_token: str = input()
-        print('')
-        print(
-            f'Ваш токен: {entered_token}? Это верно? (Для избежания дальнейших проблем с запуском, удостоверьтесь, '
-            f'что токен введён правильно) \n [Да/Нет]')
-
-        confirmation_count: int = 0
-        while confirmation_count < 2:
-            user_confirmation: str = input()
-            if user_confirmation.lower() in ['да', 'нет']:
-                if user_confirmation.lower() == 'да':
-                    token_count += 3
-                    break
-                else:
-                    print('')
-                    break
-            else:
-                print('Введите (Да) или (Нет)')
+        entered_token = input('Enter your token: ')
+        logging.debug(f'Token entered: {entered_token}')
+        user_confirmation = input(f'Your token: {entered_token}? Is this correct? (Yes/No) ')
+        if user_confirmation.lower() == 'yes':
+            token_count += 3
+            logging.info('Token confirmed by user')
+        else:
+            logging.warning('Token confirmation failed')
 
     with open(os.path.join(turnoff_path, 'token'), 'w') as f:
         f.write(entered_token)
+        logging.info('Token written to file')
 
-    print('Первоначальная настройка завершена.')
-    time.sleep(5)
-    console.clear()
+    logging.info('Initial setup completed')
+
+
+if __name__ == "__main__":
+    auto()
