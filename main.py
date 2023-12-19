da7067159d1dce4efa0235a06b404b3b3796dffa
danyala1 <d.mayasov@mail.ru>
2023-12-19 17:16:05 +0400

    Добавил логирование через метод

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from typing import List
 import csv
 import logging
-from logging.handlers import RotatingFileHandler
-
-logger = logging.getLogger("HeroesJuornalLogger")
-logger.setLevel(logging.INFO)
-handler = RotatingFileHandler(
-    "Heroes_juornal.log", maxBytes=5000000, backupCount=5)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
-)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 
 class Heroes:
@@ -20,7 +9,20 @@ class Heroes:
         Constructor of the Hero class.
         """
         self.heroes: List[dict] = []
-        logger.info("Heroes instance created")
+        self.logger = self.set_logger()
+        self.logger.info("Heroes instance created")
+
+    def set_logger(self):
+        logger = logging.getLogger(__name__)
+        if not logger.handlers:
+            logger.setLevel(logging.INFO)
+            file_handler = logging.FileHandler("Heroes_log.log")
+            file_handler.setLevel(logging.DEBUG)
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(message)s")
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+        return logger
 
     def change_class(self, new_class: str) -> None:
         """
@@ -29,7 +31,7 @@ class Heroes:
         :param new_class: New hero class.
         """
         self.hero_class: str = new_class
-        logger.info(f"Changed the hero class to {new_class}")
+        self.logger.info(f"Changed the hero class to {new_class}")
 
     def change_hp(self, new_hp: int) -> None:
         """
@@ -38,7 +40,7 @@ class Heroes:
         :param new_hp: A new level of hero's health.
         """
         self.hp: int = new_hp
-        logger.info(f"Changed the hero's health level to {new_hp}")
+        self.logger.info(f"Changed the hero's health level to {new_hp}")
 
     def change_damage(self, new_damage: int) -> None:
         """
@@ -47,7 +49,7 @@ class Heroes:
         :param new_damage: New hero damage.
         """
         self.damage: int = new_damage
-        logger.info(f"Hero damage changed to {new_damage}")
+        self.logger.info(f"Hero damage changed to {new_damage}")
 
     def change_armor(self, new_armor: int) -> None:
         """
@@ -56,7 +58,7 @@ class Heroes:
         :param new_armor: A new level of hero armor.
         """
         self.armor: int = new_armor
-        logger.info(f"Changed the hero's armor level to {new_armor}")
+        self.logger.info(f"Changed the hero's armor level to {new_armor}")
 
     def change_lvl(self, new_lvl: int) -> None:
         """
@@ -65,7 +67,7 @@ class Heroes:
         :param new_lvl: New hero level.
         """
         self.lvl: int = new_lvl
-        logger.info(f"Hero level changed to {new_lvl}")
+        self.logger.info(f"Hero level changed to {new_lvl}")
 
     def hero_exists(self, id: int) -> bool:
         """
@@ -79,9 +81,9 @@ class Heroes:
         """
         for hero in self.heroes:
             if hero['id'] == id:
-                logger.info(f"Hero with ID: {id} successfully found")
+                self.logger.info(f"Hero with ID: {id} successfully found")
                 return True
-            logger.error("Hero with specified ID not found")
+            self.logger.error("Hero with specified ID not found")
             raise ValueError("Hero with specified ID not found")
 
     def can_survive(self, id: int) -> bool:
@@ -97,12 +99,12 @@ class Heroes:
         for hero in self.heroes:
             if hero['id'] == id:
                 if hero['lvl'] >= 5 and hero['hp'] > 80 and hero['damage'] > 25:
-                    logger.info(f"Hero with ID: {id} won")
+                    self.logger.info(f"Hero with ID: {id} won")
                     return True
                 else:
-                    logger.info(f"Hero with ID: {id} die")
+                    self.logger.info(f"Hero with ID: {id} die")
                     return False
-        logger.error("The hero with the specified ID was not found")
+        self.logger.error("The hero with the specified ID was not found")
         raise ValueError("The hero with the specified ID was not found")
 
     def add_hero(
@@ -133,7 +135,7 @@ class Heroes:
             "lvl": lvl
         }
         self.heroes.append(new_hero)
-        logger.info(f"Added a new hero with ID {id}")
+        self.logger.info(f"Added a new hero with ID {id}")
 
     def remove_hero(self, id: int) -> None:
         """
@@ -144,18 +146,18 @@ class Heroes:
         for hero in self.heroes:
             if hero['id'] == id:
                 self.heroes.remove(hero)
-                logger.info(f"Deleted hero with ID {id}")
+                self.logger.info(f"Deleted hero with ID {id}")
                 break
 
     def sort_by_hp(self) -> None:
         """ Sorts the list of heroes by health level, in descending order."""
         self.heroes.sort(key=lambda x: x['hp'], reverse=True)
-        logger.info("The list of heroes is sorted by health level")
+        self.logger.info("The list of heroes is sorted by health level")
 
     def sort_by_lvl(self) -> None:
         """ Sorts the list of heroes by level, in descending order."""
         self.heroes.sort(key=lambda x: x['lvl'], reverse=True)
-        logger.info("The list of heroes is sorted by level")
+        self.logger.info("The list of heroes is sorted by level")
 
     def read_from_csv(self, file_name: str) -> None:
         """
@@ -168,9 +170,9 @@ class Heroes:
                 reader = csv.DictReader(file)
                 for row in reader:
                     self.heros.append(row)
-            logger.info(f"Hero data uploaded from CSV file {file_name}")
+            self.logger.info(f"Hero data uploaded from CSV file {file_name}")
         except Exception as e:
-            logger.warning(f"Error reading CSV file: {file_name} - {e}")
+            self.logger.warning(f"Error reading CSV file: {file_name} - {e}")
 
     def write_to_csv(self, file_name: str) -> None:
         """
@@ -192,9 +194,10 @@ class Heroes:
                 writer.writeheader()
                 for hero in self.heroes:
                     writer.writerow(hero)
-            logger.info(f"Clients written to CSV file: {file_name}")
+            self.logger.info(f"Clients written to CSV file: {file_name}")
         except Exception as e:
-            logger.warning(f"Error writing to CSV file: {file_name} - {e}")
+            self.logger.warning(
+                f"Error writing to CSV file: {file_name} - {e}")
 
 
 if __name__ == "__main__":
