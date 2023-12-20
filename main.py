4c01f9ce7b895e3aec0f648cc66569698844b205
Chuck-man <sgold2398@gmail.com>
2023-12-20 23:26:18 +0400

    Добавление логгирования

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from emoji import emojize  # pip install emoji
+from emoji import emojize  
 import random
 from time import sleep
 import shutil
-from simple_colors import *  # pip install simple_colors
+from simple_colors import *  
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler(f".log", mode='w')
+formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def intro():
     """
@@ -11,36 +19,36 @@ def intro():
     """
     columns = shutil.get_terminal_size().columns
 
-    print(magenta("SNAKE (🐍) WATER (💧) GUN (🔫)".center(columns)))  # Printing the name of the game
+    print(magenta("SNAKE (🐍) WATER (💧) GUN (🔫)".center(columns))) 
 
-    sleep(1.5)  # Making program to sleep to print next statement after sometime
+    sleep(1.5)  
 
-    print(cyan("GAME DEVELOPED BY - 👑 AKSHAT DODHIYA 👑".center(columns)))  # Printing the name of the developer
+    print(cyan("GAME DEVELOPED BY - 👑 AKSHAT DODHIYA 👑".center(columns))) 
 
-    sleep(2)  # Making program to sleep to execute next part of the program after sometime
+    sleep(2)  
 
 
-intro()  # Calling the function intro() to introduce the game
+intro()  
 
-computer_choice, user_choice = "", ""  # Declaring variables to store choices
-computer_points, user_points, flag, chance = 0, 0, 0, 0  # Variables :
-# store points, flag = to repeat loop once again for invalid input,
-# chance = use in while loop for calculating the chances of the user
-replay = ""  # Declaring empty string to store user's choice for replay
+computer_choice, user_choice = "", "" 
+computer_points, user_points, flag, chance = 0, 0, 0, 0  
+
+
+replay = ""  
 
 
 def choices():
     """
     This function takes input of user's choice and also takes random input from the computer from list 'options'
     """
-    global computer_choice, user_choice  # Declaring variables as global to use in function
-    options = ["Snake", "Water", "Gun"]  # List of options for computer to choose randomly
-    computer_choice = random.choice(options)  # function to store random choice from list 'options'
+    global computer_choice, user_choice 
+    options = ["Snake", "Water", "Gun"]  
+    computer_choice = random.choice(options)  
 
     print("Choose:\t\tS for", emojize(":snake:"),
           "\t\tW for", emojize(":droplet:"),
-          "\t\tG for", emojize(":pistol:"))  # Printing options for user to select
-    user_choice = input().lower()  # storing input of the user in lower case
+          "\t\tG for", emojize(":pistol:"))  
+    user_choice = input().lower()  
 
 
 def results():
@@ -48,116 +56,143 @@ def results():
     This function calculates points of both computer and user and
      it also prints whether the user won or lost in that chance and flag value is also handled in this function
     """
-    global computer_points, user_points, flag  # Globalising variables for editing it's values
-    flag = 0  # Initialising the value of flag to zero in each iteration
+    global computer_points, user_points, flag 
+    flag = 0  
     if computer_choice == "Snake":
+        logger.info("The generated value is Snake")
         if user_choice == "s" or user_choice == "snake":
             print(yellow("AWWW...!!"), emojize(":neutral_face:"), yellow("\nTWO SNAKES HAD BITTEN EACH OTHER :/"))
+            logger.info("The user choiced snake")
 
         elif user_choice == "w" or user_choice == "water":
             print(red("NOOO...!!"), emojize(":crying_face:"), red("\nTHE SNAKE DRANK YOUR WATER :("))
-            computer_points += 1  # Incrementing computer_points by one
+            computer_points += 1  
+            logger.info("The user choiced water")
 
         elif user_choice == "g" or user_choice == "gun":
             print(green("YEAH...!!"), emojize(":victory_hand:"), green("\nYOU SHOT THE SNAKE BY GUN  :)"))
-            user_points += 1  # Incrementing user_points by one
+            user_points += 1  
+            logger.info("The user choiced gun")
 
         else:
-            print("!!कृपया सही विकल्प चुनें!!")  # Error message in 'HINDI' for invalid choice
-            flag = 1  # Changing flag value to iterate the loop again in same chance
+            print("!!कृपया सही विकल्प चुनें!!") 
+            flag = 1  
+            logger.warning("The user entered invalid value")
 
     elif computer_choice == "Water":
+        logger.info("The generated value is Water")
         if user_choice == "s" or user_choice == "snake":
             print(green("YEAH...!!"), emojize(":victory_hand:"), green("\nYOUR SNAKE DRANK THE WATER :)"))
-            user_points += 1  # Incrementing user_points by one
+            user_points += 1  
+            logger.info("The user choiced snake")
 
         elif user_choice == "w" or user_choice == "water":
             print(yellow("AWWW...!!"), emojize(":neutral_face:"), yellow("\nWATER IS INCREASED :/"))
+            logger.info("The user choiced water")
 
         elif user_choice == "g" or user_choice == "gun":
             print(red("NOOO...!!"), emojize(":crying_face:"), red("\nYOUR GUN SANK INTO THE WATER :("))
-            computer_points += 1  # Incrementing computer_points by one
+            computer_points += 1  
+            logger.info("The user choiced gun")
 
         else:
-            print("!!कृपया सही विकल्प चुनें!!")  # Error message in 'HINDI' for invalid choice
-            flag = 1  # Changing flag value to iterate the loop again in same chance
+            print("!!कृपया सही विकल्प चुनें!!") 
+            flag = 1  
+            logger.warning("The user entered invalid value")
 
     elif computer_choice == "Gun":
+        logger.info("The generated value is Gun")
         if user_choice == "s" or user_choice == "snake":
             print(red("NOOO...!!"), emojize(":crying_face:"), red("\nYOUR SNAKE WAS SHOT BY THE GUN :("))
-            computer_points += 1  # Incrementing computer_points by one
+            computer_points += 1  
+            logger.info("The user choiced snake")
 
         elif user_choice == "w" or user_choice == "water":
             print(green("YEAH...!!"), emojize(":victory_hand:"), green("\nYOUR WATER HAD SUNK THE GUN INTO IT :)"))
-            user_points += 1  # Incrementing user_points by one
+            user_points += 1  
+            logger.info("The user choiced water")
 
         elif user_choice == "g" or user_choice == "gun":
             print(yellow("AWWW...!!"), emojize(":neutral_face:"), yellow("\nTWO GUNS FIRED AT EACH OTHER :/"))
+            logger.info("The user choiced gun")
 
         else:
-            print("!!कृपया सही विकल्प चुनें!!")  # Error message in 'HINDI' for invalid choice
-            flag = 1  # Changing flag value to iterate the loop again in same chance
+            print("!!कृपया सही विकल्प चुनें!!")  
+            flag = 1  
+            logger.warning("The user entered invalid value")
 
 
 def replay_game():
     """
     Function to ask and store the choice of the user for replaying the game
     """
-    while 1:  # infinite loop till the user enters a valid choice
+    while 1:  
         print("DO YOU WANT TO PLAY AGAIN ? \nENTER Y FOR YES AND N FOR NO")
-        # Giving choice to the user for replaying the game
-        global replay  # Globalising the variable to edit value of main variable
-        replay = input().lower()  # Taking input in lower case string
+        
+        global replay  
+        replay = input().lower()  
 
-        # if else condition to check whether the user has entered the valid input or not
+        
         if replay == "y" or replay == "yes":
-            break  # breaking infinite loop after getting valid input
+            logger.info("The user choiced to continue the game")
+            break 
         elif replay == "n" or replay == "no":
-            break  # breaking infinite loop after getting valid input
+            logger.info("The user choiced to finished the game")
+            break  
         else:
             print(red("Please enter a valid input only"))
-            continue  # executing the loop again due to invalid input given by the user
+            logger.warning("The user entered invalid value")
+            continue  
+    logger.info("The game was replayed")
 
 
-while 1:  # Infinite loop to play the game as many times as the user wants
-    computer_points, user_points, flag, chance = 0, 0, 0, 0  # Initialising values to zero at the beginning of the game
-    computer_choice, user_choice, replay = "", "", ""  # Emptying strings at the beginning of the game
+while 1:  
+    computer_points, user_points, flag, chance = 0, 0, 0, 0  
+    computer_choice, user_choice, replay = "", "", ""  
 
     try:
         n = int(input("HOW MANY CHANCES DO YOU WANT TO PLAY ?\n"))
         if n < 1:
             print(red('Please enter only natural number', 'bold'))
+            logger.warning("The user entered unnatural number")
             continue
 
     except Exception as e:
         print(red('Please enter only natural number', 'bold'))
+        logger.warning("The user entered invalid value")
         continue
 
-    while chance < n:  # Iterating loop 'n' times for playing 'n' number of chances
-        choices()  # Calling function to take choice of the user as input
-        results()  # Calling function to calculate result of a particular chance
-        if flag == 0:  # Incrementing flag's value only if the input given by the user will be valid
+    while chance < n:  
+        choices()  
+        results()  
+        if flag == 0:  
             chance += 1
+            logger.info("The user entered valid value")
+    logger.info("The game has been completed")
 
-    # Displaying points of both computer and user
     print("\n\t\t\tYOUR SCORE :", user_points)
     print("\n\t\t\tCOMPUTER'S SCORE :", computer_points)
 
-    # Printing the final result of the game
+    
     if computer_points > user_points:
         print(red("\n\t\t\tYOU LOST THE GAME !!"), emojize(":loudly_crying_face:"), red("YOU LOST THE GAME"))
+        logger.info("The user lost the game")
     elif user_points > computer_points:
         print(green("\n\t\t\tHURRAH !!"), emojize(":smiling_face_with_sunglasses:"), green("YOU WON THE GAME"))
+        logger.info("The user won the game")
     else:
         print(yellow("\n\t\t\t!! TIE !!"), emojize(":disappointed_face:"), yellow("!! TRY AGAIN !!"))
+        logger.info("")
 
     replay = ""
 
     replay_game()
 
     if replay == "n" or replay == "no":
-        print(red("\n\t\t\tSAD TO SEE YOU GO !!", 'bold'), emojize(":disappointed_face:"))  # Printing exit statement
-        exit()  # exiting the game
+        print(red("\n\t\t\tSAD TO SEE YOU GO !!", 'bold'), emojize(":disappointed_face:"))
+        logger.info("The user has logged out of the game")
+        exit()
     if replay == "y" or replay == "yes":
         print(green("\n\t\t\tYO LET'S PLAY AGAIN", 'bold'), emojize(":smiling_face_with_smiling_eyes:"))
-        # Printing a statement and replaying the game
+        logger.info("The user decided to stay in the game")
+        
